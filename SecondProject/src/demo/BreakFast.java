350742c7e75e830255d268f7de86ab793a608e07
ksvikas424 <srinivasvikask@gmail.com>
2021-04-19 13:14:06 +0530

    Initial Commit

---
+++ SecondProject/src/demo/BreakFast.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class BreakFast {
+	String roll;
+	Drink drink;
+	
+	public BreakFast(String roll, Drink drink) {
+		super();
+		this.roll = roll;
+		this.drink = drink;
+	}
+}
